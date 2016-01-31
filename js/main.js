0617e28287278d0e280f2ef06bf289e029ef2ec8
Samuel Turner <sam@samuel-turner.co.uk>
2016-01-31 16:00:03 +0000

    Added dropdown menu for cinemas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // Set up the dropdowns
   $(".dropdown-button").click(function() {
     var $button, $menu;
     $button = $(this);
@@ -7,6 +8,20 @@ $(document).ready(function() {
     $menu.children("li").click(function() {
       $menu.removeClass("show-menu");
       $button.html($(this).html());
+      $(".dropdown-menu").attr("data-dropdown-value", $(this).attr("data-value"));
+    });
+  });
+
+  // Populate the cinema dropdown
+  var $cinemas;
+  $.ajax({
+      url: "http://www2.cineworld.co.uk/api/quickbook/cinemas?key=zgRHNzG7",
+      data: $cinemas,
+      type: "GET",
+      dataType: 'jsonp'
+  }).then(function($cinemas) {
+    $.each($cinemas.cinemas, function( index, value ) {
+      $(".cinema-dropdown-menu").append('<li data-value="' + value.id + '">' + value.name + "</li>");
     });
   });
 });
